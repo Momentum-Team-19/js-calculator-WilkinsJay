9b25167ceae75aefd9d31520752d032e9cacbecc
Jaylan Wilkins <jay.wilkins23@yahoo.com>
2023-07-20 15:32:30 -0400

    click of boxes

--- main.js
+++ main.js
@@ -2,3 +2,14 @@ console.log('connected')
 
 let boxes = document.querySelectorAll('.boxes')
 console.log(boxes)
+
+let display = document.querySelector('#display')
+console.log(display)
+
+for (let box of boxes) {
+    box.addEventListener('click', () => {
+        console.log(box.innerText)
+        let text = document.createTextNode(box.innerText)
+        display.appendChild(text)
+    })
+}
