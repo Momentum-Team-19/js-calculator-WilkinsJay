74d34126f0b7f30eccd46489b4d6abc978bcfecb
Jaylan Wilkins <jay.wilkins23@yahoo.com>
2023-07-21 14:09:59 -0400

    equals button working

--- main.js
+++ main.js
@@ -3,8 +3,26 @@ console.log('connected')
 let boxes = document.querySelectorAll('.boxes')
 console.log(boxes)
 
+
+
+// a variable is like a nickname, a shorter way to refer to a CSSMathValue
+let shaq = "Shaquille O'Neal"
+let mamba = 'Kobe Bryant'
+
+console.log(mamba)
+// this would show 'Kobe Bryant' in the console
+
 let display = document.querySelector('#display')
+// get the part of the html that has the id 'display'
+
 console.log(display)
+
+let equals = document.querySelector('#equals')
+equals.addEventListener('click', () => {
+    let answer = eval(display.innerText)
+    console.log(answer)
+    display.innerText = answer
+}) 
 
 for (let box of boxes) {
     box.addEventListener('click', () => {
@@ -13,3 +31,8 @@ for (let box of boxes) {
         display.appendChild(text)
     })
 }
+
+let clear = document.querySelector('#clear')
+clear.addEventListener('click', () => {
+    display.innerText = ''
+})
